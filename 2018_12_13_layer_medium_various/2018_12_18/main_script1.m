aa310ade5b17706b4a121ee5fc2bcc2387b9661d
mtgorori <33616505+mtgorori@users.noreply.github.com>
2018-12-18 22:00:33 +0900

    Update main_script1.m
    SAで打つようにした．
    境界深さは5 mm

--- 2018_12_13_layer_medium_various/2018_12_18/main_script1.m
+++ 2018_12_13_layer_medium_various/2018_12_18/main_script1.m
@@ -4,15 +4,15 @@ addpath(genpath('/USCTSim-master'))
 addpath(genpath('/codes/TOF_pickers'))
 IMCL_rate = linspace(0,20,21);
 frq = 5e6;
-for ii = 1
+for ii = 5
     for jj = 1:3
         myfilename = sprintf('H:/data/kwave/medium/2018_11_07_layer_medium/Layer_medium_boundary_%0.1fmm_ IMCL%d%%.mat',...
             ii,IMCL_rate(jj));
         load(myfilename)
-        param.source.point_map = cast(51,'int8');
+        param.source.point_map = cast(linspace(1,100,100),'int8');
         param.source.waveform.freq = frq;
         param.t_end = 1e-04;
-        pathname = sprintf('H:/data/kwave/result/2018_12_13_layer_medium_various/boundary_%0.1fmm_IMCL%d%%/',...
+        pathname = sprintf('H:/data/kwave/result/2018_12_13_layer_medium_various/SA/boundary_%0.1fmm_IMCL%d%%/',...
             ii,IMCL_rate(jj));
         dst_path = pathname;
         simulate_usct(param, medium, dst_path);
